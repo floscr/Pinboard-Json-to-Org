81e480caf0348dc567b5fefb90bb3ef649959b34
Florian Schroedl <hello@florianschroedl.com>
2018-11-27 21:21:24 +0100

    Add time parsing, add top level header

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const R = require('ramda');
+const D = require('date-fns');
 const fs = require('fs');
 
 const data = require('./data/pinboard_export.json');
@@ -17,19 +18,34 @@ const formatDescription = R.pipe(
   // Brackets dont play well with the org urls
   R.replace(/\[/g, "("),
   R.replace(/\]/g, ")"),
-  // Remove freaking emojis
-  R.replace(/([\uE000-\uF8FF]|\uD83C[\uDF00-\uDFFF]|\uD83D[\uDC00-\uDDFF])/g, ""),
 )
 
-const main = function() {
-  const entries = data.entries.map(function(x) {
-    return `
-** [[${x.href}][${formatDescription(x.description)}]] ${formatTags(x.tags)}
+const orgPrintEntry = function(x) {
+  return `** [[${x.href}][${formatDescription(x.description)}]] ${formatTags(x.tags)}
 :PROPERTIES:
 :time: ${x.time}
 :END:
-${x.extended}`
-  });
+${x.extended}
+`;
+}
+
+const buildEntries = R.pipe(
+  R.map(R.over(
+    R.lensProp('time'),
+    R.pipe(
+      x => D.parse(x),
+      x => D.format(x, 'YYYY-MM-DD ddd hh:mm'),
+      x => `[${x}]`,
+    ),
+  )),
+  R.sortBy(R.prop('time')),
+  R.map(orgPrintEntry),
+  R.join(""),
+  x => `* Entries\n${x}`,
+)
+
+const main = function() {
+  const entries = buildEntries(data.entries)
   fs.writeFile("entries.org", entries, function(err) {
     if (err) return console.log(err);
     console.log("The file was saved!");
