2a96664914443ccc64c0f436f272e318b5f93a01
Florian Schroedl <hello@florianschroedl.com>
2018-11-27 20:22:06 +0100

    Add main file

---
+++ index.js
@@ -0,0 +1,26 @@
+const R = require('ramda');
+const fs = require('fs');
+
+const data = require('./data/pinboard_export.json');
+
+const formatTags = R.pipe(
+  R.replace(/\s/g, ":"),
+  x => `:${x}:`,
+);
+
+const main = function() {
+  const entries = data.entries.map(function(x) {
+    return `
+** [[${x.href}][${x.description}]] ${formatTags(x.tags)}
+  :PROPERTIES:
+  :time: ${x.time}
+  :END:
+  ${x.extended}`
+  });
+  fs.writeFile("entries.org", entries, function(err) {
+    if (err) return console.log(err);
+    console.log("The file was saved!");
+  });
+}
+
+main();
