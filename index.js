39cab6bfbb27df7402c10e55d5d099ab2194718a
Florian Schroedl <hello@florianschroedl.com>
2018-11-27 20:39:05 +0100

    Format description

--- index.js
+++ index.js
@@ -13,14 +13,22 @@ const formatTags = R.when(
   ),
 )
 
+const formatDescription = R.pipe(
+  // Brackets dont play well with the org urls
+  R.replace(/\[/g, "("),
+  R.replace(/\]/g, ")"),
+  // Remove freaking emojis
+  R.replace(/([\uE000-\uF8FF]|\uD83C[\uDF00-\uDFFF]|\uD83D[\uDC00-\uDDFF])/g, ""),
+)
+
 const main = function() {
   const entries = data.entries.map(function(x) {
     return `
-** [[${x.href}][${x.description}]] ${formatTags(x.tags)}
-  :PROPERTIES:
-  :time: ${x.time}
-  :END:
-  ${x.extended}`
+** [[${x.href}][${formatDescription(x.description)}]] ${formatTags(x.tags)}
+:PROPERTIES:
+:time: ${x.time}
+:END:
+${x.extended}`
   });
   fs.writeFile("entries.org", entries, function(err) {
     if (err) return console.log(err);
